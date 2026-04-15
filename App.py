4034ea06c190577e4a1cb689022c6dd9ded15d03
eshwari13808-png <eshwari13808@gmail.com>
2026-04-15 11:49:05 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,16 +4,19 @@ from groq import Groq
 st.set_page_config("EshwariAI Contect Generator", layout="wide")
 st.title("EshwariAI-Contect Generator")
 st.image("download.jfif")
+# Get GROQ API Key
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
-product = st.text_input("Productr")
-audience = st.text_inout("Audience")
-if st.button("Generate Contect"):
-  prompt = f"Write marketing contect for {product} targeting {audience}."
-  response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
-    meassages=[{"role": "user", "contect": prompt}]
-  )
-      st.session_state.text = response.choices[0].message.content
+# Get Product Name and Audience for That Product
+product = st.text_input("Product")
+audience = st.text_input("Audience")
+# Button to Generate Content
+if st.button("Generate Content"):
+    prompt = f"Write marketing content for {product} targeting {audience}."
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    st.session_state.text = response.choices[0].message.content
     text =response.choices[0].message.content
     st.write(text)
 # After Content Create - Download The File
@@ -25,5 +28,5 @@ if "text" in st.session_state:
             file_name="marketing_copy.txt",
             mime="text/plain"
         )
-  else:
-      st.info("Generate content first")
+    else:
+        st.info("Generate content first")
